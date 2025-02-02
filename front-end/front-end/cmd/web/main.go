a51b1a8483e02bc590549a187b753511ca498346
mahesh <maheshh.mahii.98@gmail.com>
2025-02-02 15:09:22 -0800

    front end code to hit the broker service

--- front-end/front-end/cmd/web/main.go
+++ front-end/front-end/cmd/web/main.go
@@ -11,9 +11,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
-
-	fmt.Println("Starting front end service on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting front end service on port 8081")
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		log.Panic(err)
 	}
